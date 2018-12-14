653b8e428901ec7d910abecff3ee92a5abd389bb
Wellington Felix <wfmf@cin.ufpe.br>
2018-12-14 06:53:34 -0300

    Refatora funcoes de cadastro de geofence

--- app/src/main/java/com/wehack/cinlocation/database/ReminderManager.kt
+++ app/src/main/java/com/wehack/cinlocation/database/ReminderManager.kt
@@ -3,11 +3,13 @@ package com.wehack.cinlocation.database
 import com.wehack.cinlocation.model.Reminder
 
 interface ReminderManager {
-    fun findById(id: Long): Reminder
+    fun findById(id: Long): Reminder?
 
-    fun getAll(): List<Reminder>
+    fun getAll(): List<Reminder>?
 
-    fun insert(reminder: Reminder): Long
+    fun insert(reminder: Reminder): Long?
+
+    fun update(reminder: Reminder): Reminder
 
     fun delete(vararg reminder: Reminder)
 }
